f793669dbf4055f46b6810bca5143449b2b7ba10
Gilberto Avalos <avalosagnostic@gmail.com>
2014-05-22 07:19:25 -0500

    Added tabs and tables

--- application/views/layout.php
+++ application/views/layout.php
@@ -2,21 +2,34 @@
 <html>
     <meta charset="utf-8"/>
     <head>
-        <title><?php $title_for_layout ?></title>
+        <title>PHP Test</title>
 
         <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
         <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
     </head>
     <body ng-app="app">
 
         <!-- Content -->
-        <?php echo $content_for_layout ?>
+        <div class="container">
+
+            <?php echo $content_for_layout ?>
+
+            <footer>
+                <div class="row">
+                  <div class="col-lg-12">
+                    <p>Made by <a href="http://github.com/Agnostic">Gilberto Avalos</a> (<a href="mailto:avalosagnostic@gmail.com">avalosagnostic@gmail.com</a>).</p>
+                    <p>Based on <a href="http://getbootstrap.com" rel="nofollow">Bootstrap</a>. Icons from <a href="http://fortawesome.github.io/Font-Awesome/" rel="nofollow">Font Awesome</a>.
+
+                  </div>
+                </div>
+            </footer>
+        </div>
 
         <!-- Scripts -->
         <script type="text/javascript" src="assets/js/libs/jquery-1.10.2.min.js"></script>
         <script type="text/javascript" src="assets/js/libs/bootstrap.min.js"></script>
-        <script type="text/javascript" src="assets/js/libs/angujar.min.js"></script>
+        <script type="text/javascript" src="assets/js/libs/angular.min.js"></script>
         <script type="text/javascript" src="assets/js/app.js"></script>
     </body>
 </html>
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,12 +1,96 @@
-<div class="container">
-
-	<div class="page-header" id="banner">
-		<div class="row">
-		  <div class="col-lg-6">
-		    <h1>Lumen</h1>
-		    <p class="lead">Light and shadow</p>
-		  </div>
+<div class="page-header">
+	<div class="row">
+		<div class="col-lg-6">
+			<h1>PHP Test</h1>
+			<p class="lead">This is an example project to demostrate my skills of HTML, CSS, PHP and JavaScript.</p>
 		</div>
 	</div>
 
+	<div>
+
+		<!-- Tabs -->
+		<ul class="nav nav-tabs" style="margin-bottom: 15px;">
+			<li class="active">
+				<a href="#prospects" data-toggle="tab">Prospects</a>
+			</li>
+			<li>
+				<a href="#customers" data-toggle="tab">Customers</a>
+			</li>
+		</ul>
+
+		<!-- Tab content -->
+		<div class="tab-content">
+
+			<!-- Prospects -->
+			<div class="tab-pane fade active in" id="prospects">
+				<table class="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>State</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Gilberto Avalos</td>
+							<td>NV</td>
+							<td>
+								<button type="button" class="btn btn-default">Convert to Customer</button>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Gilberto Avalos</td>
+							<td>NV</td>
+							<td>
+								<button type="button" class="btn btn-default">Convert to Customer</button>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Gilberto Avalos</td>
+							<td>NV</td>
+							<td>
+								<button type="button" class="btn btn-default">Convert to Customer</button>
+							</td>
+						</tr>
+						<tr>
+							<td>1</td>
+							<td>Gilberto Avalos</td>
+							<td>NV</td>
+							<td>
+								<button type="button" class="btn btn-default">Convert to Customer</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+			<!-- Customers -->
+			<div class="tab-pane fade" id="customers">
+				<table class="table table-striped table-hover">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Name</th>
+							<th>State</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Gilberto Avalos</td>
+							<td>NV</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+
+		</div>
+	</div>
 </div>
