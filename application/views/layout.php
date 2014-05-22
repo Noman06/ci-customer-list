1e8558861838fc6a3a3ddbc1a373d8248ae09393
Gilberto Avalos <avalosagnostic@gmail.com>
2014-05-22 06:52:11 -0500

    Added bootstrap, jquery and angularjs

--- application/views/layout.php
+++ application/views/layout.php
@@ -3,10 +3,20 @@
     <meta charset="utf-8"/>
     <head>
         <title><?php $title_for_layout ?></title>
+
+        <!-- Styles -->
         <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
     </head>
-    <body>
+    <body ng-app="app">
+
+        <!-- Content -->
         <?php echo $content_for_layout ?>
+
+        <!-- Scripts -->
+        <script type="text/javascript" src="assets/js/libs/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="assets/js/libs/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/js/libs/angujar.min.js"></script>
+        <script type="text/javascript" src="assets/js/app.js"></script>
     </body>
 </html>
