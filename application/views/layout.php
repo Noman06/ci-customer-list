7ab843ec7560b32787cf177f93afc719f8bca4fa
Gilberto Avalos <avalosagnostic@gmail.com>
2014-05-22 06:42:11 -0500

    Added project files

---
+++ application/views/layout.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <meta charset="utf-8"/>
+    <head>
+        <title><?php $title_for_layout ?></title>
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+    </head>
+    <body>
+        <?php echo $content_for_layout ?>
+    </body>
+</html>
